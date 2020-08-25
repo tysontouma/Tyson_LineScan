ef4ea3a027cc7ef58423051effa4e72637261442
Tyson <tysontouma98@gmail.com>
2020-08-25 15:36:48 -0700

    Met with tatsu, he guided me!
    all changes are from meeting with tatsu and bettering the code

--- string_to_list_for_loop.py
+++ string_to_list_for_loop.py
@@ -8,6 +8,7 @@ list_of_xs = []
 list_of_ts_w_xt = []
 list_of_ts_w_xs = []
 
+counter = 0
 with open('ten_lines.txt', "r") as txt:
     for line in txt:
         row = line.strip("\n").split("\t")
@@ -15,41 +16,39 @@ with open('ten_lines.txt', "r") as txt:
         t = t_in_l / freq
         x = float(row[1])
         y = float(row[2])
-        if t == t_main:
-            # list_of_xs = []
-            if abs(abs(y) - abs(y_relative)) <= tol:
-                ts_w_xs = [t , x]
-                list_of_ts_w_xs.append(ts_w_xs)
-                list_of_xs.append(x)
-                if t == t_max: # PROBLEM AREA
-                    avg_x = sum(list_of_xs) / len(list_of_xs)
-                    ts_w_xt = [t, avg_x]
-                    list_of_ts_w_xt.append(ts_w_xt)
 
-                # this is useless because takes average for every line
-                # avg_x = sum(list_of_xs) / len(list_of_xs)
-                # list_of_xt.append(avg_x)
-                # avg_x = 0
+        if t > t_max:  # PROBLEM AREA
+            break
+
+        if abs(t - t_main) < 0.00000001:
+            # list_of_xs = []
+            if abs(y - y_relative) <= tol:
+                list_of_ts_w_xs.append([t, x])
+                list_of_xs.append(x)
+
         # this is when it moves from 0 sec to 0.02 sec
         else:
             avg_x = sum(list_of_xs) / len(list_of_xs)
             ts_w_xt = [t_main, avg_x]
             list_of_ts_w_xt.append(ts_w_xt)
-            avg_x = 0
             list_of_xs = []
 
-            t_main = t_main + 1/freq
+            t_main = t_main + 1/freq # counter/freq, counter =+
             # i wrote this whole section again because it skips the first entry of 0.02 sec
-            if abs(abs(y) - abs(y_relative)) <= tol:
-                ts_w_xs = [t , x]
-                list_of_ts_w_xs.append(ts_w_xs)
+            if abs(y - y_relative) <= tol:
+                list_of_ts_w_xs.append([t, x])
                 list_of_xs.append(x)
-                if t == t_max: #PROBLEM AREA
-                    avg_x = sum(list_of_xs) / len(list_of_xs)
-                    ts_w_xt = [t, avg_x]
-                    list_of_ts_w_xt.append(ts_w_xt)
+
+        counter = counter + 1
+
+avg_x = sum(list_of_xs) / len(list_of_xs)
+ts_w_xt = [t, avg_x]
+list_of_ts_w_xt.append(ts_w_xt)
+
 
 print(list_of_ts_w_xt)
 print()
 print(list_of_ts_w_xs)
+
+test = 1
 
